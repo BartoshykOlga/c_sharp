dc830d414a66997adf2b3010f0e7efe6ff558aa9
Bartoshyk Olga <eireen@mail.ru>
2024-01-27 19:33:41 +0300

    task4

---
+++ lesson1/task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 18;
+int b = 3;
+int c = 8;
+int d = 14;
+int e = 28;
+
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
