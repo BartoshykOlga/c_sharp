5bbf1fe7511900451d92a53122a24c16a2354278
Bartoshyk Olga <eireen@mail.ru>
2024-01-27 18:59:02 +0300

    task2

--- lesson1/task2/Program.cs
+++ lesson1/task2/Program.cs
@@ -7,12 +7,18 @@ long l1 = 6;
 long sum2 = l1+i2;
 //Console.WriteLine(sum2);
 
-double d1 = 5.5;
+double d1 = 6;
 double sum3 = d1 + i1;
 
 int product1 = i1*i2;
 long product2 = l1*i1;
 double product3 = d1*i1;
-Console.WriteLine(product1);
-Console.WriteLine(product2);
-Console.WriteLine(product3);
+//Console.WriteLine(product1);
+//Console.WriteLine(product2);
+//Console.WriteLine(product3);
+
+int quotient = i1/i2;
+//Console.WriteLine(quotient);
+
+double quotient2 = i1/d1;
+Console.WriteLine(quotient2);
